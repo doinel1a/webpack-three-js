a8c028eaa16f08b873c3717d226ec51549cccc58
doinel1a <doinel1atanasiu@gmail.com>
2023-03-04 22:55:19 +0100

    feat(scene): scene with a sphere implemented

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,15 @@ import '../css/global.css';
 import '../scss/global.scss';
 
 import _config from '../../_config.cjs';
+import Three from './three';
 
 const githubCornerTitle = 'Get started on GitHub';
-const githubRepoUrl = 'https://github.com/doinel1a/vanilla-js-starter';
+const githubRepoUrl = 'https://github.com/doinel1a/wp-vanilla-three-js-starter';
 
 document.addEventListener('DOMContentLoaded', () => {
   const title = document.querySelector('#title');
   const main = document.querySelector('#main');
   const githubCorner = main.querySelector('#github');
-  const counter = main.querySelector('#counter');
 
   title.textContent = _config.title;
 
@@ -18,22 +18,5 @@ document.addEventListener('DOMContentLoaded', () => {
   githubCorner.setAttribute('aria-label', githubCornerTitle);
   githubCorner.setAttribute('href', githubRepoUrl);
 
-  main.addEventListener('click', (event) => {
-    const { id, type } = event.target;
-
-    if (type === 'button') {
-      const currentCount = Number(counter.textContent);
-
-      switch (id) {
-        case 'increment': {
-          counter.textContent = currentCount + 1;
-          break;
-        }
-        case 'decrement': {
-          counter.textContent = currentCount - 1;
-          break;
-        }
-      }
-    }
-  });
+  new Three(document.querySelector('#canvas'));
 });
---
+++ src/js/three.js
@@ -0,0 +1,102 @@
+import * as T from 'three';
+
+import aoMap from '../assets/textures/ao-map.jpg';
+import metalnessMap from '../assets/textures/metalness-map.jpg';
+import normalMap from '../assets/textures/normal-map.jpg';
+import roughnessMap from '../assets/textures/roughness-map.jpg';
+
+const device = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: window.devicePixelRatio
+};
+
+export default class Three {
+  constructor(canvas) {
+    this.scene = new T.Scene();
+
+    this.camera = new T.PerspectiveCamera(
+      75,
+      device.width / device.height,
+      0.1,
+      100
+    );
+    this.camera.position.set(0, 0, 3);
+    this.scene.add(this.camera);
+
+    this.renderer = new T.WebGLRenderer({
+      canvas,
+      alpha: true,
+      antialias: true,
+      preserveDrawingBuffer: true
+    });
+    this.renderer.setSize(device.width, device.height);
+    this.renderer.setPixelRatio(Math.min(device.pixelRatio, 2));
+
+    this.textureLoader = new T.TextureLoader();
+
+    this.clock = new T.Clock();
+
+    this.setLights();
+    this.setGeometry();
+    this.render();
+    this.setResize();
+  }
+
+  setLights() {
+    this.topPointLight = new T.PointLight(0x7000ff, 2);
+    this.topPointLight.position.set(0, 2.5, -0.5);
+    this.scene.add(this.topPointLight);
+
+    this.middlePointLight = new T.PointLight(0xffffff, 0.025);
+    this.middlePointLight.position.set(0, 0, 2);
+    this.scene.add(this.middlePointLight);
+
+    this.bottomPointLight = new T.PointLight(0xb2ff, 2);
+    this.bottomPointLight.position.set(0, -2.5, -0.5);
+    this.scene.add(this.bottomPointLight);
+  }
+
+  setGeometry() {
+    this.sphereGeometry = new T.SphereGeometry(1, 64, 64);
+
+    this.sphereMaterial = new T.MeshStandardMaterial({
+      color: new T.Color(0xffffff)
+    });
+    this.sphereMaterial.aoMapIntensity = 1;
+    this.sphereMaterial.aoMap = this.textureLoader.load(aoMap);
+    this.sphereMaterial.roughness = 5;
+    this.sphereMaterial.roughnessMap = this.textureLoader.load(roughnessMap);
+    this.sphereMaterial.metalness = 2;
+    this.sphereMaterial.metalnessMap = this.textureLoader.load(metalnessMap);
+    this.sphereMaterial.normalMap = this.textureLoader.load(normalMap);
+
+    this.sphereMesh = new T.Mesh(this.sphereGeometry, this.sphereMaterial);
+    this.scene.add(this.sphereMesh);
+  }
+
+  render() {
+    const elapsedTime = this.clock.getElapsedTime();
+
+    this.sphereMesh.rotation.x = 0.3 * elapsedTime;
+    this.sphereMesh.rotation.y = 0.6 * elapsedTime;
+
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  setResize() {
+    window.addEventListener('resize', this.onResize.bind(this));
+  }
+
+  onResize() {
+    device.width = window.innerWidth;
+    device.height = window.innerHeight;
+
+    this.camera.aspect = device.width / device.height;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(device.width, device.height);
+    this.renderer.setPixelRatio(Math.min(device.pixelRatio, 2));
+  }
+}
