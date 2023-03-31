7bffbceb6ce2216b5a41974215c4ae3a0adb63d5
doinel1a <doinel1atanasiu@gmail.com>
2023-03-31 19:32:36 +0200

    chore: host & port on browsers.test file

--- tests/browsers.test.js
+++ tests/browsers.test.js
@@ -1,9 +1,11 @@
 /* eslint-disable unicorn/prefer-module */
 // @ts-check
+import _config from '../_config.cjs';
+
 const { test } = require('@playwright/test');
 
-const HOST = 'http://localhost';
-const PORT = 3000;
+const HOST = _config.server.host;
+const PORT = _config.server.port;
 
 test('Test browsers', async ({ page }) => {
   await page.goto(`${HOST}:${PORT}`);
