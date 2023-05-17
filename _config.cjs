0485bc904e42c6aa492e84dc053ea7d2dafce74a
doinel1a <doinel1atanasiu@gmail.com>
2023-05-17 07:37:34 +0200

    refactor(index.html): keywords

--- _config.cjs
+++ _config.cjs
@@ -8,7 +8,13 @@ module.exports = {
     title: 'Webpack ThreeJS — Template',
     description:
       'Boilerplate template designed to quickly bootstrapping a 3D Web App, SPA, website or landing page with Three.JS, GLSL, Webpack, TailwindCSS, ESLint, Husky and much more in just 30 seconds.',
-    keywords: `three, threejs, three-js, webgl, 3D, GLSL, GLSL shaders, shaders, web3D javascript, js, vanilla-javascript, vanilla-js, development, template, boilerplate, template-boilerplate, boilerplate-frontend, starter-kit, webpack, babel, core-js, husky, eslint, commitlint, prettier, postcss, tailwind, tailwindcss, sass, scss, css, github, web app, web-app, webapp, spa 
+    keywords: `
+      three, three js, threejs, three.js, three-js,
+      three template, three js template, threejs template, three.js template, three-js template, 
+      three webpack, three js webpack, threejs webpack, three.js webpack, three-js webpack,
+      three webpack template, three js webpack templante, threejs webpack templante, three.js webpack templante, three-js webpack templante,
+      webgl, 3D, GLSL, GLSL shaders, shaders, web3D, 
+      javascript, js, vanilla-javascript, vanilla-js, development, template, boilerplate, template-boilerplate, boilerplate-frontend, starter-kit, webpack, babel, core-js, husky, eslint, commitlint, prettier, postcss, tailwind, tailwindcss, sass, scss, css, github, web app, web-app, webapp, spa 
     `
   }
 };
